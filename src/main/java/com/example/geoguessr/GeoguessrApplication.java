3906af1b17e2a9e4167b2f651ee9153d907cead0
Matej <matej.novosad.mn@gmail.com>
2022-08-26 21:53:40 +0200

    Initial commit

---
+++ src/main/java/com/example/geoguessr/GeoguessrApplication.java
@@ -0,0 +1,13 @@
+package com.example.geoguessr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoguessrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoguessrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/geoguessr/GeoguessrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.geoguessr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoguessrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
